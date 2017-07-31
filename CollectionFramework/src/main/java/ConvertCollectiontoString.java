384a237ff443dd831b540e1c76be1e45aab57acd
arun789 <adwiti1975@gmail.com>
2017-07-31 00:46:35 -0700

    convert collection to a string

--- CollectionFramework/src/main/java/ConvertCollectiontoString.java
+++ CollectionFramework/src/main/java/ConvertCollectiontoString.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by Adwiti on 7/31/2017.
@@ -27,6 +28,11 @@ public class ConvertCollectiontoString {
 
         String strSetToStringWithPipe = StringUtils.collectionToDelimitedString(strSet,"|");
         System.out.println("set to String using pipe " + strSetToStringWithPipe);
+
+        System.out.println("use of Java 8 to convert collection to a string");
+
+        String converttoAStrng = strLst.stream().map(v -> v.toString()).collect(Collectors.joining("|"));
+        System.out.println("using Java 8 to convert to String " + converttoAStrng);
 
     }
 }
