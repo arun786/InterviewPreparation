0ec8a93036f4750bb9686f1b169a90c22236e75b
arun789 <adwiti1975@gmail.com>
2017-07-30 20:19:01 -0700

    sorting a map before java 8

---
+++ CollectionFramework/src/main/java/MapFundasForSortingAMapByKeyBeforeJava8.java
@@ -0,0 +1,57 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by Adwiti on 7/30/2017.
+ */
+public class MapFundasForSortingAMapByKeyBeforeJava8 {
+
+    public static void main(String[] args) {
+        Map<String, String> unsortedMap = addValuesToMap();
+        printMap(unsortedMap);
+        System.out.println("After Sorting...");
+        Map<String, String> sortedMap = sortMapUsingTreeMap(unsortedMap);
+        printMap(sortedMap);
+
+        System.out.println("sorting hashmap in descending order ");
+        sortedMap = sortMapInDescendingOrder(unsortedMap);
+        printMap(sortedMap);
+
+    }
+
+    public static Map<String, String> addValuesToMap() {
+        Map<String, String> maps = new HashMap<>();
+        maps.put("one", "Mon");
+        maps.put("two", "Tue");
+        maps.put("five", "Fri");
+        maps.put("six", "Sat");
+        maps.put("three", "Wed");
+        maps.put("four", "Thur");
+        return maps;
+    }
+
+    public static void printMap(Map<String, String> maps) {
+        maps.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        });
+    }
+
+    public static Map<String, String> sortMapUsingTreeMap(Map<String, String> unsortedMap) {
+        Map<String, String> sortedMap = new TreeMap<>(unsortedMap);
+        return sortedMap;
+    }
+
+    public static Map<String, String> sortMapInDescendingOrder(Map<String, String> unsortedMap) {
+        Map<String, String> sortedMap = new TreeMap<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        });
+
+        sortedMap.putAll(unsortedMap);
+        return sortedMap;
+    }
+}
