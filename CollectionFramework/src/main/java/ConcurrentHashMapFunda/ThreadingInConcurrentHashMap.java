3b90822c01397bdbf9a39fe57a023148334931a3
arun789 <adwiti1975@gmail.com>
2017-08-14 00:01:51 -0700

    concurrent hash map explained

--- CollectionFramework/src/main/java/ConcurrentHashMapFunda/ThreadingInConcurrentHashMap.java
+++ CollectionFramework/src/main/java/ConcurrentHashMapFunda/ThreadingInConcurrentHashMap.java
@@ -7,7 +7,32 @@ import java.util.concurrent.ConcurrentHashMap;
  * Created by Adwiti on 8/13/2017.
  */
 public class ThreadingInConcurrentHashMap extends Thread {
-    private static Map<Integer,String> map = new ConcurrentHashMap<>();
+    private static Map<Integer, String> map = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) {
+        System.out.println("Adding values to hash Map with main thread...");
+        map.put(1, "one");
+        map.put(2, "two");
+        ThreadingInConcurrentHashMap t = new ThreadingInConcurrentHashMap();
+        System.out.println("Starting main Thread...");
+        t.start();
+        map.forEach((k, v) -> {
+            System.out.print(k + " = " + v + " ");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        /*o/p will be
+        Adding values to hash Map with main thread...
+        Starting main Thread...
+        Child Thread iterating...
+        1 = one Adding values to Concurrent Hash Map...
+        {1=one, 2=two, 3=three}
+        2 = two 3 = three*/
+    }
 
     @Override
     public void run() {
@@ -15,27 +40,10 @@ public class ThreadingInConcurrentHashMap extends Thread {
         try {
             Thread.sleep(2000);
             System.out.println("Adding values to Concurrent Hash Map...");
-            map.put(3,"three");
+            map.put(3, "three");
             System.out.println(map);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Adding values to hash Map with main thread...");
-        map.put(1,"one");
-        map.put(2,"two");
-        ThreadingInConcurrentHashMap t = new ThreadingInConcurrentHashMap();
-        System.out.println("Starting main Thread...");
-        t.start();
-        map.forEach((k,v) -> {
-            System.out.print( k + " = " + v + " ");
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
     }
 }
